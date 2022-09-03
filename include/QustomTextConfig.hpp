48ba5c3f7ac067d4b701abfffbc02bac75debe76
Nuggo <NuggoDEV@gmail.com>
2022-09-04 00:10:32 +0930

    Forgot to add the Multiplayer Toggle smh

--- include/QustomTextConfig.hpp
+++ include/QustomTextConfig.hpp
@@ -6,22 +6,22 @@ DECLARE_CONFIG(QustomTextConfig,
 
     CONFIG_VALUE(ModToggle, bool, "Disable Qustom Text", false);
 
-    CONFIG_VALUE(SoloToggle, bool, "Disable Custom Solo Text", false);
-    CONFIG_VALUE(MultiToggle, bool, "Disable Custom Multiplayer Text", false);
-    CONFIG_VALUE(CampaignToggle, bool, "Disable Custom Campaign Text", false);
-    CONFIG_VALUE(PartyToggle, bool, "Disable Custom Party Text", false);
+    CONFIG_VALUE(SoloToggle, bool, "Enable Custom Solo Text", false);
+    CONFIG_VALUE(MultiToggle, bool, "Enable Custom Multiplayer Text", false);
+    CONFIG_VALUE(CampaignToggle, bool, "Enable Custom Campaign Text", false);
+    CONFIG_VALUE(PartyToggle, bool, "Enable Custom Party Text", false);
 
-    CONFIG_VALUE(FailToggle, bool, "Disable Custom Fail Text", false);
-    CONFIG_VALUE(PassToggle, bool, "Disable Custom Pass Text", false);
+    CONFIG_VALUE(FailToggle, bool, "Enable Custom Fail Text", false);
+    CONFIG_VALUE(PassToggle, bool, "Enable Custom Pass Text", false);
 
 
     CONFIG_VALUE(SoloText, std::string, "Solo Button Text", "SOLO");
-    CONFIG_VALUE(MultiText, std::string, "Solo Button Text", "SOLO");
-    CONFIG_VALUE(CampaignText, std::string, "Solo Button Text", "SOLO");
-    CONFIG_VALUE(PartyText, std::string, "Solo Button Text", "SOLO");
+    CONFIG_VALUE(MultiText, std::string, "Multiplayer Button Text", "MULTIPLAYER");
+    CONFIG_VALUE(CampaignText, std::string, "Campaign Button Text", "CAMPAIGN");
+    CONFIG_VALUE(PartyText, std::string, "Party Button Text", "PARTY");
 
-    CONFIG_VALUE(FailText, std::string, "Solo Button Text", "SOLO");
-    CONFIG_VALUE(PassText, std::string, "Solo Button Text", "SOLO");
+    CONFIG_VALUE(FailText, std::string, "Fail Screen Text", "LEVEL FAILED");
+    CONFIG_VALUE(PassText, std::string, "Pass Screem Text", "LEVEL CLEARED");
 
 
     CONFIG_INIT_FUNCTION(
